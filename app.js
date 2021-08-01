02ee9a4762a4c8bea7781d7a78708adb6222a7e1
Bene-Hur Pessoa <benehurp@gmail.com>
2021-08-01 10:45:01 -0300

    Iniciando Home

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'home.html'));
+});
+
+app.get('/home', (req, res) => {
+  res.redirect('/');
+});
+
 app.listen(3000, () => {
-  console.log("O servidor está rodando na porta 3000");
+  console.log('O servidor está rodando na porta 3000');
 });
