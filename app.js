7bcc4e283f459a3f9117b5e11703666e0dea9367
Thiago Lins <taglins@gmail.com>
2021-08-01 22:18:01 -0300

    Rotas iniciais criadas

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const path = require('path');
-app.use(express.static(path.join(__dirname, 'public')));
+const path = require("path");
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'views', 'home.html'));
+app.get("/home", (req, res) => {
+  res.redirect("/");
+});
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "home.html"));
 });
 
-app.get('/home', (req, res) => {
-  res.redirect('/');
+app.get("/produto/:id?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "product.html"));
+});
+app.get("/categoria/:id?", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "category.html"));
+});
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "login.html"));
+});
+app.get("/account", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "account.html"));
 });
 
 app.listen(3000, () => {
-  console.log('O servidor está rodando na porta 3000');
+  console.log("O servidor está rodando na porta 3000");
 });
