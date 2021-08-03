0a5b224f53aaee5f8369a9928beb601cb9a33cbf
Thiago Lins <taglins@gmail.com>
2021-08-03 20:31:34 -0300

    Correção de Erros EJS

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const express = require("express");
 const app = express();
-//Configurando EJS
-app.set("view engine", "ejs");
-app.set("views", "./src/views");
-
 // importando módulo path
 const path = require("path");
 // importando as rotas principais
 const rotas = require("./src/routes");
 app.use(rotas);
+//Configurando EJS
+app.set("view engine", "ejs");
+app.set("views", path.resolve("src", "views"));
 //liberando acesso a pasta public
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.resolve("public")));
 // instanciando o server
 app.listen(3000, () => {
   console.log("O servidor está rodando na porta 3000");
--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -2,22 +2,22 @@
 const path = require("path");
 const mainController = {
   index: (req, res) => {
-    res.render("pages/home");
+    res.render("pages/home", { page: "Home" });
   },
   indexRedirect: (req, res) => {
     res.redirect("/");
   },
   product: (req, res) => {
-    res.render("pages/product");
+    res.render("pages/product", { page: "Produto" });
   },
   category: (req, res) => {
-    res.render("pages/category");
+    res.render("pages/category", { page: "Categoria" });
   },
   login: (req, res) => {
-    res.render("pages/login");
+    res.render("pages/login", { page: "Login" });
   },
   account: (req, res) => {
-    res.render("pages/account");
+    res.render("pages/account", { page: "Minha Conta" });
   },
 };
 module.exports = mainController;
