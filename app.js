45609fb145e4ab26e7c310ba453fb2d28015cd35
Bene-Hur Pessoa <benehurp@gmail.com>
2021-08-10 21:30:57 -0300

    Ajustes na Home HTML e CSS

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 //Definindo porta
 const port = 3000;
 // importando módulo path
-const path = require("path");
+const path = require('path');
 // importando as rotas principais
-const rotas = require("./src/routes");
+const rotas = require('./src/routes');
 app.use(rotas);
 //Configurando EJS
-app.set("view engine", "ejs");
-app.set("views", path.resolve("src", "views"));
+app.set('view engine', 'ejs');
+app.set('views', path.resolve('src', 'views'));
 //liberando acesso a pasta public
-app.use(express.static(path.resolve("public")));
+app.use(express.static(path.resolve('public')));
 // instanciando o server
 app.listen(port, () => {
   console.log(`O servidor está rodando em http://localhost:${port}`);
--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,27 +1,27 @@
 //importando módulo path
-const path = require("path");
+const path = require('path');
 const mainController = {
   index: (req, res) => {
-    res.sendFile(path.resolve(__dirname, "..", "views", "home.html"));
-    // res.render("pages/home", { page: "Home" });
+    res.sendFile(path.resolve(__dirname, '..', 'views', 'home.html'));
+    res.render('pages/home', { page: 'Home' });
   },
   indexRedirect: (req, res) => {
-    res.redirect("/");
+    res.redirect('/');
   },
   product: (req, res) => {
-    res.render("pages/product", { page: "Produto" });
+    res.render('pages/product', { page: 'Produto' });
   },
   category: (req, res) => {
-    res.render("pages/category", { page: "Categoria" });
+    res.render('pages/category', { page: 'Categoria' });
   },
   loginTest: (req, res) => {
-    res.sendFile(path.resolve(__dirname, "..", "views", "login.html"));
+    res.sendFile(path.resolve(__dirname, '..', 'views', 'login.html'));
   },
   account: (req, res) => {
-    res.render("pages/account", { page: "Minha Conta" });
+    res.render('pages/account', { page: 'Minha Conta' });
   },
   login: (req, res) => {
-    res.render("pages/login", { page: "Login" });
+    res.render('pages/login', { page: 'Login' });
   },
 };
 module.exports = mainController;
