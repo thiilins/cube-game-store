556c1a5e9af850deef01787edffa79f26686dd16
Thiago Lins <taglins@gmail.com>
2021-08-19 19:52:57 -0300

    Atualização pagina Cart/Product

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,7 +10,5 @@ router.get("/login", MainController.login);
 router.get("/loginTest", MainController.loginTest);
 router.get("/minha-conta", MainController.account);
 router.get("/checkout", MainController.checkout);
-router.get("/header", MainController.headertest);
 router.get("/cart", MainController.cart);
 module.exports = router;
-
