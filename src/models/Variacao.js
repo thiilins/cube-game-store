68c8e288d811fc1d06ebce8956f02254bb25bfcf
Thiago Lins <taglins@gmail.com>
2021-10-25 01:16:40 -0300

    Corrigindo conflit de  propriedade

--- src/models/Variacao.js
+++ src/models/Variacao.js
@@ -54,7 +54,7 @@ module.exports = (connection, DataTypes) => {
     });
     Variacao.hasMany(models.ItemPedido, {
       foreignKey: "variacao_id",
-      as: "vendas",
+      as: "vendidos",
       onDelete: "cascade",
     });
   };
