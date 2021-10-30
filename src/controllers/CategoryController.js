340e2e7a6a36af2f60ee0b53115a70a1a1d98160
ruancorreia <correia.ruan@gmail.com>
2021-10-29 21:35:47 -0300

    Feature: Categorias

--- src/controllers/CategoryController.js
+++ src/controllers/CategoryController.js
@@ -1,19 +1,19 @@
+const { Categoria, Usuario, Produto } = require("../models");
+
 const CategoryController = {
   createCategoryPage(req, res) {
-    try {
-      return res.render("admin/dashboard", {
-        file: "categories/create",
-        page: "Adicionar Categoria",
-      });
-    } catch (error) {
-      console.log(error);
-    }
+    return res.render("admin/dashboard", {
+      file: "categories/create",
+      page: "Adicionar Categoria",
+    });
   },
   async listCategoryPage(req, res) {
     try {
+      const categories = await Categoria.findAll();
       return res.render("admin/dashboard", {
         file: "categories/list",
         page: "Categorias",
+        categories,
       });
     } catch (error) {
       console.log(error);
@@ -43,6 +43,27 @@ const CategoryController = {
   },
   async editCategory(req, res) {
     try {
+      const { id } = req.params;
+      const { nome } = req.body;
+      let img_url = undefined;
+      if (req.file && req.files != "undefined") {
+        const { filename } = req.file;
+        img_url = filename;
+      }
+      const category = await Categoria.update(
+        {
+          nome,
+          img_url,
+        },
+        {
+          where: { id },
+        }
+      );
+      return res.status(200).json({
+        category,
+        msg: "Categoria Editada Com Sucesso",
+      });
+      // return res.redirect("/admin/categorias");
     } catch (error) {
       console.log(error);
     }
