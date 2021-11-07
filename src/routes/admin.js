769b3c09e61d7be6566d6fac481bb98f6eb8709f
Thiago Lins <taglins@gmail.com>
2021-11-07 01:17:30 -0300

    Adição das rotas de fabricante

--- src/routes/admin.js
+++ src/routes/admin.js
@@ -7,6 +7,7 @@ const OrderController = require("../controllers/OrderController");
 const ProductController = require("../controllers/ProductController");
 const UserController = require("../controllers/UserController");
 const CategoryController = require("../controllers/CategoryController");
+const ManufacturerController = require("../controllers/ManufacturerController");
 
 // router.use(isLogin);
 // router.use(acessLevel);
@@ -35,6 +36,15 @@ router.get("/categorias/criar", CategoryController.createCategoryPage);
 router.get("/categorias/editar/:id", CategoryController.editCategoryPage);
 router.get("/categorias/:id", CategoryController.viewCategoryPage);
 
+//FABRICANTES
+router.get("/fabricantes", ManufacturerController.listManufacturerPage);
+router.get("/fabricantes/criar", ManufacturerController.createManufacturerPage);
+router.get(
+  "/fabricantes/editar/:id",
+  ManufacturerController.editManufacturerPage
+);
+router.get("/fabricantes/:id", ManufacturerController.viewManufacturerPage);
+
 // PEDIDO
 router.get("/pedidos", OrderController.listOrderPage);
 router.get("/pedidos/criar", OrderController.createOrderPage);
@@ -58,6 +68,11 @@ router.delete("/produto/:id", ProductController.deleteProduct);
 router.post("/categoria/", CategoryController.createCategory);
 router.put("/categoria/:id", CategoryController.editCategory);
 router.delete("/categoria/:id", CategoryController.deleteCategory);
+
+//CATEGORIA
+router.post("/fabricante/", ManufacturerController.createManufacturer);
+router.put("/fabricante/:id", ManufacturerController.editManufacturer);
+router.delete("/fabricante/:id", ManufacturerController.deleteManufacturer);
 //PEDIDO
 router.post("/pedido/", OrderController.createOrder);
 router.put("/pedido/:id", OrderController.editOrder);
