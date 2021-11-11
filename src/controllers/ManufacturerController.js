eb1843839ce6d0e9c0b0fde3d9844aa35148d614
Lucas Sodré <lukassodre21@gmail.com>
2021-11-11 19:58:35 -0300

    Manufacturer Feature(incomplete)

--- src/controllers/ManufacturerController.js
+++ src/controllers/ManufacturerController.js
@@ -1,11 +1,15 @@
+const {Fabricante, Categoria, Produto} = require("../models")
+
+
 const ManufacturerController = {
   createManufacturerPage(req, res) {
     try {
       res.status(200).json();
-      // return res.render("admin/dashboard", {
+      return res.render("/fabricantes/criar", {
+      // Em qual página vai o fabricante mesmo?
       //   file: "categories/create",
       //   page: "Adicionar Categoria",
-      // });
+      });
     } catch (error) {
       console.log(error);
       res
@@ -15,32 +19,43 @@ const ManufacturerController = {
   },
   async listManufacturerPage(req, res) {
     try {
-      // return res.render("admin/dashboard", {
+      return res.render("fabricantes", {
       //   file: "categories/list",
       //   page: "Categorias",
-      // });
+      });
     } catch (error) {
       console.log(error);
     }
   },
   async viewManufacturerPage(req, res) {
-    // res.render("admin/dashboard", {
+    res.render("fabricantes/:id", {
     //   file: "categories/view",
     //   page: "",
-    // });
+    });
   },
   async editManufacturerPage(req, res) {
     try {
-      // return res.render("admin/dashboard", {
+      return res.render("fabricantes/editar/:id", {
       //   file: "categories/edit",
       //   page: "Editando Categoria:",
-      // });
+      });
     } catch (error) {
       console.log(error);
     }
   },
+
+  //Dúvida em relação à criação de novo fabricante;
+
   async createManufacturer(req, res) {
     try {
+
+      const {
+        nome,
+        img_url,
+        ativo
+      } = req.body;
+
+
       res.status(201).json();
     } catch (error) {
       console.log(error);
